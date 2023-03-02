971f2a34d6547e8ddc545f97b0ef496517bbce5c
mattivaisanen <vaisanen.ma@gmail.com>
2023-03-02 16:04:33 +0200

    fixed increment 2, had too much code

--- mygrep2/main.cpp
+++ mygrep2/main.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    
 
     if(argc==1){
         string text, filename, search_string;
@@ -25,88 +26,27 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    else if(argc == 3 || argc == 4) {
+    else if(argc == 3) {
+        string search_string = argv[1];
+        string filename = argv[2];
+        ifstream file(filename);
+
+        if (!file.is_open()) {
+            cerr << "Error opening file " << filename << endl;
+            return 1;
+        }
+
         string line;
-
-        if(argc == 3){
-            cout << "Arguments 3";
-            string search_string = argv[1];
-            string filename = argv[2];
-            ifstream file(filename);
-
-
-            while (getline(file, line)) {
-                if (line.find(search_string) != string::npos) {
-                    cout << line << endl;
-                }
+        while (getline(file, line)) {
+            if (line.find(search_string) != string::npos) {
+                cout << line << endl;
             }
         }
-
-        if(argc == 4){
-            cout << "Arguments 4";
-
-            int linecount = 0;
-            int line_number = 1;
-
-            string options = argv[1];
-            string search_string = argv[2];
-            string filename = argv[3];
-
-            ifstream file(filename);
-
-            bool linenumber_option;
-            bool occurrance_option;
-
-            for (int i = 2; i < options.size()/sizeof(options[0]); i++) {
-                if (options[i] == 'l') {
-                    cout << "has L" << endl;
-                    linenumber_option = true;
-                }
-
-                if (options[i] == 'o') {
-                    cout << "has O" << endl;
-                    occurrance_option = true;
-                }
-            }
-
-            cout << "1";
-
-            if(occurrance_option == true && linenumber_option == true){
-                while (getline(file, line)) {
-                    if (line.find(search_string) != string::npos) {
-                        cout << line << endl;
-                        linecount++;
-                    }
-                    line_number++;
-                }
-                cout << "Occurrances of lines containing " << search_string << ": " << linecount;
-            }
-            else{
-                if(linenumber_option){
-                    while (getline(file, line)) {
-                        if (line.find(search_string) != string::npos) {
-                            cout << line_number << ": " << line << endl;
-                        }
-                        line_number++;
-                    }
-                }
-
-                if(occurrance_option){
-                    while (getline(file, line)) {
-                        if (line.find(search_string) != string::npos) {
-                            cout << line << endl;
-                            linecount++;
-                        }
-                    }
-                    cout << "Occurrances of lines containing " << search_string << ": " << linecount;
-                }
-            }
-        }
-
+        
     }
 
     else {
-        cerr << "Usage: mygrep <options> <search string> <filename>" << endl;
+        cerr << "Usage: " << "mygrep " << " <search string> <filename>" << endl;
         return 1;
     }
 
